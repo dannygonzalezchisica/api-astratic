fe48754068c19b2484bc84d17fb0e673b6d71e7c
dannygonzalezchisica <dannychisica@gmail.com>
2021-10-11 21:33:00 -0500

    agregar controlador para la creación de usuarios, falta capturar el error de campos obligatorios

--- views/usuarios/rutas.js
+++ views/usuarios/rutas.js
@@ -1,48 +1,24 @@
 import Express from "express"
+import { queryAllUsers, crearUsuario } from "../../controllers/usuarios/controller.js"
 import { getDB } from "../../db/db.js"
 
 const rutasUsuario = Express.Router()
 
-rutasUsuario.route('/usuarios').get((req, res) => {
-    console.log('Alguien hizo GET en la ruta /usuarios')
-    const baseDeDatos = getDB()
-    baseDeDatos.collection('usuario').find({}).toArray((err, result)=> {
+const genericCallback = (res) => (err, result) => {
         if(err) {
             res.status(500).send('Error consultando los vehículos')
         } else {
             res.json(result)
         }
-    })
+}
+
+rutasUsuario.route('/usuarios').get((req, res) => {
+    console.log('Alguien hizo GET en la ruta /usuarios')
+    queryAllUsers(genericCallback(res))
 })
 
 rutasUsuario.route('/usuarios/nuevo').post((req, res) => {
-    const datosUsuario = req.body
-    console.log("Llaves: ", Object.keys(datosUsuario))
-    try {
-        if (Object.keys(datosUsuario).includes('documentoIdentidad') &&
-        Object.keys(datosUsuario).includes('nombre') &&
-        Object.keys(datosUsuario).includes('apellidos') &&
-        Object.keys(datosUsuario).includes('numeroCelular') &&
-        Object.keys(datosUsuario).includes('correoElectronico') &&
-        Object.keys(datosUsuario).includes('rol') &&
-        Object.keys(datosUsuario).includes('estado')) {
-            // Implementar código para crear usuario en la BD
-            const baseDeDatos = getDB()
-            baseDeDatos.collection('usuario').insertOne(datosUsuario, (err, result)=> {
-                if(err) {
-                    console.error(err)
-                   res.sendStatus(500) 
-                } else {
-                    console.log(result)
-                    res.sendStatus(200)
-                }
-            })
-        } else {
-            res.sendStatus(500)
-        }
-    } catch {
-        res.sendStatus(500)
-    }
+    crearUsuario(req.body, genericCallback(res))
 })
 
 rutasUsuario.route('/usuarios/editar').patch((req, res) => {
