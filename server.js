827f9b003de65ccc155e54802d2f5421dd6a5676
dannygonzalezchisica <dannychisica@gmail.com>
2021-10-13 07:51:43 -0500

    agregar rutas y controladoes para las ventas y los productos

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import Cors from "cors";
 import dotenv from "dotenv"
 import { conectarBD } from "./db/db.js";
 import rutasUsuario from "./views/usuarios/rutas.js";
+import rutasProducto from "./views/productos/rutas.js";
+import rutasVenta from "./views/ventas/rutas.js";
 
 dotenv.config({path: './.env'})
 
@@ -11,6 +13,8 @@ const app = Express()
 app.use(Express.json())
 app.use(Cors())
 app.use(rutasUsuario)
+app.use(rutasProducto)
+app.use(rutasVenta)
 
 const main = () => {
     return app.listen(process.env.PORT, () => {
