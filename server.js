17ef7a22066ccc9c6d3f262e839d2075c4ca78c2
dannygonzalezchisica <dannychisica@gmail.com>
2021-10-14 21:51:52 -0500

    agregar autenticación de Auth0 en el backend

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ import { conectarBD } from "./db/db.js";
 import rutasUsuario from "./views/usuarios/rutas.js";
 import rutasProducto from "./views/productos/rutas.js";
 import rutasVenta from "./views/ventas/rutas.js";
+import jwt from "express-jwt";
+import jwks from "jwks-rsa";
 
 dotenv.config({path: './.env'})
 
@@ -12,6 +14,21 @@ const app = Express()
 
 app.use(Express.json())
 app.use(Cors())
+
+var jwtCheck = jwt({
+    secret: jwks.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: 'https://astra-tic-empresa.us.auth0.com/.well-known/jwks.json'
+  }),
+  audience: 'api-autenticacion-astra-tic',
+  issuer: 'https://astra-tic-empresa.us.auth0.com/',
+  algorithms: ['RS256']
+});
+
+app.use(jwtCheck);
+
 app.use(rutasUsuario)
 app.use(rutasProducto)
 app.use(rutasVenta)
